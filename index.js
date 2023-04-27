5709b79ea23d7483ad611925b95a11f0d9797eb4
UgoDigle <ugo.digle@gmail.com>
2023-04-27 13:40:13 +0200

    màj JS et fin de complication

--- index.js
+++ index.js
@@ -1,108 +1,83 @@
-// document.addEventListener("submit", search);
+document.querySelector("#codepostal").addEventListener("input", rechercheCP);
+document.querySelector("form").addEventListener("submit", rechercheNomVille);
 
-// function search(event) {
-//   event.preventDefault();
-//   const cpInput = document.querySelector("#codepostal").value;
-//   const cpUrl = `https://geo.api.gouv.fr/communes?codePostal=${cpInput}`;
-//   const cityInput = document.querySelector("#nomville").value;
-//   const cityUrl = `https://geo.api.gouv.fr/communes?nom=${cityInput}`;
+function rechercheCP(event){
+    event.preventDefault();
+    const cpInput = document.querySelector("#codepostal").value;
+    const cpUrl = `https://geo.api.gouv.fr/communes?codePostal=${cpInput}`;
+    fetch(cpUrl)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        if(data.length === 0){
+        document.querySelector(".résultat").textContent = "Code postal non trouvé";
+        return;
+        }
+        showData(data);                
+        const nomCommune = data[0].nom;
+        document.querySelector("#nomville").value = nomCommune;
+    })
+    .catch((error) => console.error(error));
+}
 
-//   let url = "";
-//   if (cpInput !== "") {
-//     url = cpUrl;
-//   } else if (cityInput !== "") {
-//     url = cityUrl;
-//   }
+function rechercheNomVille(event){
+    event.preventDefault();
+    document.querySelector("#liste_ville").innerHTML="";
+    const cityInput = document.querySelector("#nomville").value;
+    const cityUrl = `https://geo.api.gouv.fr/communes?nom=${cityInput}`;
+    fetch(cityUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        showData(data);
+      })
+      .catch((error) => console.error(error));
+  }
 
-//   if (url !== "") {
-//     fetch(url)
-//       .then((response) => response.json())
-//       .then((data) => {
-//         console.log(data);
-//         showData(data);
-//       })
-//       .catch((error) => console.error(error));
-//   }
-// }
-
-// function showData(data) {
-//   const cp = data[0].codesPostaux;
-//   const ville = data[0].nom;
-
-//   document.querySelector(".résultat").textContent = cp + ", " + ville;
-// }
-// const codePostalInput = document.getElementById('codepostal');
-// const resultatOutput = document.querySelector('.résultat');
-
-// codePostalInput.addEventListener('input', () => {
-//   const codePostal = codePostalInput.value.trim();
-
-//   if (codePostal.length === 5) {
-//     fetch(`https://geo.api.gouv.fr/communes?codePostal=${codePostal}&fields=nom,codesPostaux`)
-//       .then(response => response.json())
-//       .then(data => {
-//         if (data.length > 0) {
-//           resultatOutput.textContent = data[0].nom;
-//         } else {
-//           resultatOutput.textContent = "Aucune ville trouvée";
-//         }
-//       })
-//       .catch(error => console.error(error));
-//   } else {
-//     resultatOutput.textContent = "";
-//   }
-// });
-// let resultat = "";
-// // Fonction pour effectuer la recherche par code postal
-// function rechercheParCodePostal(codePostal) {
-//   // URL de l'API de l'INSEE pour la recherche par code postal
-//   const url = `https://geo.api.gouv.fr/communes?codePostal=${codePostal}`;
   
-//   // Requête GET à l'API de l'INSEE
-//   fetch(url)
-//     .then(response => response.json())
-//     .then(data => {
-//       // Récupération du nom de la ville et de son code postal
-//       const ville = data[0].nom;
-//       const codePostal = cp = data[0].codesPostaux;
-      
-//       // Affichage du résultat dans le champ "#résultat"
-//       const resultat = document.querySelector("#résultat");
-//       resultat.textContent = `${ville} (${codePostal})`;
-//     })
-//     .catch(error => console.error(error));
-// }
+  const select = document.querySelector("#liste_ville")
 
-// // Fonction pour effectuer la recherche par nom de ville
-// function rechercheParNomVille(nomVille) {
-//   // URL de l'API de l'INSEE pour la recherche par nom de ville
-//   const url = `https://geo.api.gouv.fr/communes?nom=${nomVille}`;
-  
-//   // Requête GET à l'API de l'INSEE
-//   fetch(url)
-//     .then(response => response.json())
-//     .then(data => {
-//       // Récupération du nom de la ville et de son code postal
-//       const ville = data[0].nom;
-//       const codePostal = data[0].codesPostaux;
-      
-//       // Affichage du résultat dans le champ "#résultat"
-//       const resultat = document.querySelector("#résultat");
-//       resultat.textContent = `${ville} (${codePostal})`;
-//     })
-//     .catch(error => console.error(error));
-// }
+  function showData(data) {
+    while (select.hasChildNodes()) {
+        select.removeChild(select.lastChild);
+      }
+    const communes = data.map(d => d.nom);
+    const cp = data[0].codesPostaux;
+    select.style.visibility = 'visible';
+    console.log(select);
+    communes.forEach(commune => {
+    const option = document.createElement("option");
+    option.textContent = commune;
+    select.appendChild(option);
+    });
+    const resultat = document.querySelector(".résultat");
+    resultat.innerHTML = "";
+    if (cp.length === 1) {
+    resultat.textContent = cp[0];
+    } else {
+    const ul = document.createElement("ul");
+    cp.forEach(codePostal => {
+    const li = document.createElement("li");
+    li.textContent = codePostal;
+    ul.appendChild(li);
+    });
+    resultat.appendChild(ul);
+    }
+    resultat.appendChild(select);
+    // document.querySelector("#codepostal").value="";
+    // document.querySelector("form").value="";
 
-// // Surveillance de l'input codepostal
-// const codePostalInput = document.querySelector("#codepostal");
-// codePostalInput.addEventListener("input", event => {
-//   const codePostal = event.target.value;
-//   rechercheParCodePostal(codePostal);
-// });
+    }
 
-// // Surveillance de l'input nomville
-// const nomVilleInput = document.querySelector("#nomville");
-// nomVilleInput.addEventListener("input", event => {
-//   const nomVille = event.target.value;
-//   rechercheParNomVille(nomVille);
-// });
+// select.setAttribute("onclick", "nomville");
+// select.addEventListener("change", majCP)
+
+// function majCP(){
+//     console.log("ok");
+
+
+//     const selectedCommune = this.value;
+//     const selectedData = data.find(d => d.nom === selectedCommune);
+//     const selectedCp = selectedData.codesPostaux[0];
+//     document.querySelector(".cp-resultat").textContent = selectedCp;
+// };
